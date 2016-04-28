9c0010af515a22530630637236e44249a703c9a8
Jo Kakomba <ariel.kakomba@gmail.com>
2016-04-28 06:52:50 +0000

    first commit

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,34 @@
+class ServicesController < ApplicationController
+  def index
+    @services = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+
+    if @service.save
+      redirect_to @service
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def service_params
+      params.require(:service).permit(:title, :description, :price, :requirements)
+    end
+end
